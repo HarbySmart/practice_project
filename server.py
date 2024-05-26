76429311b442b0e7cf8482f5871f4c35c3936012
HarbySmart <155396836+HarbySmart@users.noreply.github.com>
2024-05-26 23:21:24 +0100

    updated

--- server.py
+++ server.py
@@ -22,17 +22,16 @@ def sent_analyzer():
     response = sentiment_analyzer(text_to_analyze)
     label = response['label']
     score = response['score']
-
-    return "The text given has been identified as {} with a score of {}.".format(label.split('_')[1], score)
+    if label is None:
+        return "Invalid input! Try again."
+    return f"The given text has been identified as {label.split('_')[1]} with a score of {score}."
 
 @app.route("/")
 def render_index_page():
-    ''' This function initiates the rendering of the main application
-        page over the Flask channel
-    '''
+    #This function initiates the rendering of the main application page over the Flask channel
     return render_template('index.html')
 
 if __name__ == "__main__":
-    ''' This functions executes the flask app and deploys it on localhost:5000
-    '''
+    #This functions executes the flask app and deploys it on localhost:5000
+
     app.run(host="0.0.0.0", port=5000)
